c27a08ee88874d4413ab634e8f18982e83c751fc
Stuti Saha <b519054@iiit-bh.ac.in>
2021-09-21 18:26:36 +0530

    First Commit

---
+++ js/thankyou.js
@@ -0,0 +1,27 @@
+const ok_button = document.querySelector(".btn");
+
+
+const heading = document.querySelector(".payment-success");
+const discription = document.querySelector(".payment-discription");
+// const redirect = document.querySelector(".redirecting");
+const container = document.querySelector(".success-box");
+const timeline = new TimelineMax({ delay: 0.5 });
+
+window.onload = () => {
+    if (window.innerWidth < 600) {
+        timeline.fromTo(container, 0.5, { width: "0%", opacity: 0 }, { width: "80%", opacity: 1 });
+    } else {
+        timeline.fromTo(container, 0.5, { width: "0%", opacity: 0 }, { width: "50%", opacity: 1 });
+    }
+    timeline.fromTo(heading, 0.5, { y: -50, opacity: 0 }, { y: 0, opacity: 1 });
+    timeline.fromTo(discription, 0.5, { opacity: 0 }, { opacity: 1 });
+    timeline.fromTo(ok_button, 0.5, { opacity: 0 }, { opacity: 1 });
+
+    // setInterval(function() {
+    //     window.location.replace("index.html")
+    // }, 8000)
+}
+
+ok_button.addEventListener('click', function() {
+    window.location.href = "index.html";
+}); 
